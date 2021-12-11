4b48fa6e2b883f783da2bfaa5b546f59a145bbad
AlbaCasas <albacasasarzua1b@gmail.com>
2021-12-11 08:57:03 +0100

    log results

--- 06-challenge/script.js
+++ 06-challenge/script.js
@@ -9,3 +9,8 @@ const prices = [125, 555, 44];
 const tips = [calcTip(prices[0]), calcTip(prices[1]), calcTip(prices[2])];
 
 const total = [prices[0] + tips[0], prices[1] + tips[1], prices[2] + tips[2]];
+console.log({
+  prices,
+  tips,
+  total,
+});
