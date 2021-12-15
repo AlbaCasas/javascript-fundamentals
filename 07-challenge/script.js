f3535eec54843a31834ed40a032f472a72241ee6
AlbaCasas <albacasasarzua1b@gmail.com>
2021-12-15 12:17:40 +0100

    Create objetcs

---
+++ 07-challenge/script.js
@@ -0,0 +1,13 @@
+const BMI = mass / height ** 2;
+const mark = {
+  firstName: "Mark",
+  lastName: "Miller",
+  Mass: 78,
+  Height: 1.69,
+};
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  Mass: 92,
+  Height: 1.95,
+};
